970904c23a5765dc49b4bdd626b78cbd1dc8f4eb
xuan7zhang <xzhan567@student.ubc.ca>
2026-02-08 00:15:51 +0000

    Deploying to gh-pages from @ xuan7zhang/xuan7izhang.github.io@7343dbc49e7444c7f368bf618598cf4a43ae0873 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "xuan7zhang/xuanqizhang.github.io",
+    "data-repo": "xuan7zhang/xuan7izhang.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
